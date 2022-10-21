a94f89dd2338d0a7e38883905dbee56a1512d08a
Alejandro <al225vh@student.lnu.se>
2022-10-21 11:58:37 +0200

    Add getter for display component

--- src/js/components/stand-outify-display/stand-outify-display.js
+++ src/js/components/stand-outify-display/stand-outify-display.js
@@ -9,6 +9,7 @@ template.innerHTML = `
 <style>
 /*Style here.*/
 </style>
+<h2 id="h2-title"></h2>
 <div id="display-container">
 </div>
 `
@@ -19,6 +20,13 @@ customElements.define('stand-outify-display',
    */
   class extends HTMLElement {
     /**
+     * The display container.
+     *
+     * @type {DivElement}
+     */
+    #displayContainer
+
+    /**
      * Creates an instance of the current type.
      */
     constructor() {
@@ -28,12 +36,45 @@ customElements.define('stand-outify-display',
       // append the template to the shadow root.
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
+
+      this.#displayContainer = this.shadowRoot.getElementById('display-container')
     }
 
     /**
      * Called after the element is inserted into the DOM.
      */
     connectedCallback() {
+      // Select the node that will be observed for mutations
+      const targetNode = document.getElementById('some-id');
+
+      // Options for the observer (which mutations to observe)
+      const config = { attributes: false, childList: true, subtree: true };
+
+      // Callback function to execute when mutations are observed
+      const callback = (mutationList, observer) => {
+        for (const mutation of mutationList) {
+          if (mutation.type === 'childList') {
+            console.log('A child node has been added or removed.');
+          }
+        }
+      }
+
+      // Create an observer instance linked to the callback function
+      const observer = new MutationObserver(callback);
+
+      // Start observing the target node for configured mutations
+      observer.observe(targetNode, config);
+
+      // Later, you can stop observing
+      observer.disconnect();
+
+    }
+
+    /**
+  * Gets the element option value.
+ */
+    get displayContainer() {
+      return this.#displayContainer
     }
   }
 )
