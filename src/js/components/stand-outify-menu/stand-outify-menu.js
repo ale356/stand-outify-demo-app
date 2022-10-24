4922171ca5b5d486649be175174d3571fbff59b7
Alejandro <al225vh@student.lnu.se>
2022-10-24 12:20:29 +0200

    Add css to menu

--- src/js/components/stand-outify-menu/stand-outify-menu.js
+++ src/js/components/stand-outify-menu/stand-outify-menu.js
@@ -10,8 +10,53 @@ const template = document.createElement('template')
 template.innerHTML = `
 <style>
 /*Style here.*/
+select::-ms-expand {
+  display: none;
+}
+select {
+  display: inline-block;
+  box-sizing: border-box;
+  padding: 0.5em 2em 0.5em 0.5em;
+  border: 1px solid #eee;
+  font: inherit;
+  line-height: inherit;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -ms-appearance: none;
+  appearance: none;
+  background-repeat: no-repeat;
+  background-image: linear-gradient(45deg, transparent 50%, currentColor 50%), linear-gradient(135deg, currentColor 50%, transparent 50%);
+  background-position: right 15px top 1em, right 10px top 1em;
+  background-size: 5px 5px, 5px 5px;
+}
+.select-menu {
+  font-size: 20px;
+}
+/* From uiverse.io */
+button {
+  padding: 1.3em 3em;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 500;
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+}
+
+button:hover {
+  background-color: #23c483;
+  box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+  color: #fff;
+}
 </style>
   <div class="select-menu">
+  <h3>Choose an element</h3>
     <select id="element-options">
       <option selected value="button">Button</option>
       <option value="a">Anchor</option>
--- src/js/components/stand-outify/stand-outify.js
+++ src/js/components/stand-outify/stand-outify.js
@@ -8,6 +8,21 @@ const template = document.createElement('template')
 template.innerHTML = `
 <style>
 /*Style here.*/
+button {
+  padding: 1.3em 3em;
+  font-size: 12px;
+  text-transform: uppercase;
+  letter-spacing: 2.5px;
+  font-weight: 500;
+  color: #000;
+  background-color: #fff;
+  border: none;
+  border-radius: 45px;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease 0s;
+  cursor: pointer;
+  outline: none;
+}
 #slot-element {
   display: flex;
   align-items: center;
