39bc4afd9b2bd147c8063fdcba8c551e56b70748
Alejandro <al225vh@student.lnu.se>
2022-10-24 16:28:34 +0200

    Improve code quality in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,47 +8,40 @@ import './components/stand-outify/index.js'
 import './components/stand-outify-menu/index.js'
 import './components/stand-outify-display/index.js'
 
-// Create the custom elements.
 const standOutifyMenuElement = document.createElement('stand-outify-menu')
 const standOutifyDisplayElement = document.createElement('stand-outify-display')
 
-// Append the elements to the document.
 const headerContainer = document.querySelector('.header')
 const middleContainer = document.querySelector('.middle')
 
 headerContainer.append(standOutifyMenuElement)
 middleContainer.append(standOutifyDisplayElement)
 
-// Add a event listener to the dom.
 const standOutifyMenuReference = document.querySelector('stand-outify-menu')
 
+// Listen after the button animate, and then display an animted element.
 standOutifyMenuReference.addEventListener('animateElement', (event) => {
 
-  // Check if there already are a standoutify element existing.
   const standOutifyDisplayReference = document.querySelector('stand-outify-display')
   const standOutifyElementExists = standOutifyDisplayReference.shadowRoot.querySelector('stand-outify')
-  console.log(standOutifyElementExists)
 
   if (standOutifyElementExists != null) {
     standOutifyElementExists.remove()
   }
-
-  const standOutifyElement = document.createElement('stand-outify')
-  document.body.append(standOutifyElement)
 
   // Get the selected values and animate the element.
   const elementOptionValue = standOutifyMenuElement.elementOptionValue
   const animationOptionValue = standOutifyMenuElement.animationOptionValue
   const elementToAnimate = document.createElement(elementOptionValue)
   elementToAnimate.textContent = 'Demo'
+
+  const standOutifyElement = document.createElement('stand-outify')
   standOutifyElement.initializeElement(animationOptionValue, elementToAnimate, 'click')
 
-  // Add the animated element as a child to the display component.
   const standOutifyDisplayContainer = document.querySelector('stand-outify-display').shadowRoot.getElementById('display-container')
-  const standOutifyElementReference = document.querySelector('stand-outify')
-  standOutifyDisplayContainer.appendChild(standOutifyElementReference)
+  standOutifyDisplayContainer.appendChild(standOutifyElement)
 
-  // Change the property none to flex.
+  // Show the display container.
   standOutifyDisplayContainer.style.display = 'flex'
 
 })
