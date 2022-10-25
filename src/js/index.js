f60bfa88e8adf926691e86e256cecd4a23e5ddcd
Alejandro <al225vh@student.lnu.se>
2022-10-25 11:34:51 +0200

    Add functions removeOldStandOutify and getCurrentData

--- src/js/index.js
+++ src/js/index.js
@@ -23,30 +23,24 @@ const standOutifyMenuReference = document.querySelector('stand-outify-menu')
 /*
 1. It adds an event listener that runs when custom event fires.
 2. The code inside the function does.
-3. Checks if there is a stand outify element - standOutifyExists() returns boolean value.
-4. Gets the current values from user. - getCurrentData() - returns arraylist of strings.
-5. Creates an element and animates it. - createDemoElement() - returns standOutifyElement.
+3. Checks if there is a stand outify element - standOutifyExists() returns boolean value. - Done
+4. Gets the current values from user. - getCurrentData() - returns arraylist of strings. - Done
+5. Creates an element and animates it. - createDemoElement() - returns standOutifyElement. - 
 6. Assigns the stand outify element to the display container. - ShowDemoElement() - returns void
 Keep the add eventlistener but lift out the code to separate functions.
 
 */
 standOutifyMenuReference.addEventListener('animateElement', (event) => {
 
-  const standOutifyDisplayReference = document.querySelector('stand-outify-display')
-  const standOutifyElementExists = standOutifyDisplayReference.shadowRoot.querySelector('stand-outify')
+  removeOldStandOutify()
 
-  if (standOutifyElementExists != null) {
-    standOutifyElementExists.remove()
-  }
+  const currentDataArray = getCurrentData()
 
-  // Get the selected values and animate the element.
-  const elementOptionValue = standOutifyMenuElement.elementOptionValue
-  const animationOptionValue = standOutifyMenuElement.animationOptionValue
-  const elementToAnimate = document.createElement(elementOptionValue)
+  const elementToAnimate = document.createElement(currentDataArray[0])
   elementToAnimate.textContent = 'Demo'
 
   const standOutifyElement = document.createElement('stand-outify')
-  standOutifyElement.initializeElement(animationOptionValue, elementToAnimate, 'click')
+  standOutifyElement.initializeElement(currentDataArray[1], elementToAnimate, 'click')
 
   const standOutifyDisplayContainer = document.querySelector('stand-outify-display').shadowRoot.getElementById('display-container')
   standOutifyDisplayContainer.appendChild(standOutifyElement)
@@ -56,7 +50,7 @@ standOutifyMenuReference.addEventListener('animateElement', (event) => {
 
 })
 
-const removeOldStandOutify () => {
+const removeOldStandOutify = () => {
   const standOutifyDisplayReference = document.querySelector('stand-outify-display')
   const standOutifyElementExists = standOutifyDisplayReference.shadowRoot.querySelector('stand-outify')
 
@@ -64,3 +58,14 @@ const removeOldStandOutify () => {
     standOutifyElementExists.remove()
   }
 }
+
+const getCurrentData = () => {
+  const elementOptionValue = standOutifyMenuElement.elementOptionValue
+  const animationOptionValue = standOutifyMenuElement.animationOptionValue
+  const currentDataArray = []
+  currentDataArray.push(elementOptionValue)
+  currentDataArray.push(animationOptionValue)
+  return currentDataArray
+}
+
+// Test functions here.
